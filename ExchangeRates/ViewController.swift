96bfb1e6923f25d5e7fb78e1aec34727c890f88c
AndAndrew <andrew.krivokhizhin@gmail.com>
2020-05-19 14:35:39 +0400

    Initial Commit

---
+++ ExchangeRates/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExchangeRates
+//
+//  Created by Andrew K on 19.05.2020.
+//  Copyright © 2020 Andrew K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
